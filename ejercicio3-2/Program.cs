520d2189c6ee31ff25d906abc64f9d53e80133a1
SANTIAGO <SANTIAGO@LAPTOP-REHH1RB2>
2022-11-13 19:34:44 -0600

    Agregar archivos de proyecto.

---
+++ ejercicio3-2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ejercicio32
+{
+
+    class program
+    {
+
+        static void Main(String[] args)
+        {
+
+            //determinar si el valor de un numero es positivo o negativo 
+            Console.WriteLine("ingrese un numero");
+            int numero = int.Parse(Console.ReadLine());
+            if (numero == 0)
+            {
+
+                Console.WriteLine("el numero" + numero + "es un numero neutro");
+
+
+            }
+            else if (numero > 0)
+            {
+                Console.WriteLine("el numero" + numero + "es un numero positivo");
+            }
+            else
+            {
+                Console.WriteLine("el numero" + numero + "es un numero negativo");
+            }
+            Console.ReadKey();
+        }
+
+
+    }
+}
